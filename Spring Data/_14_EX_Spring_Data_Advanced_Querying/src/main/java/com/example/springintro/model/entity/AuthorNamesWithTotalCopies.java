04c1f639d042397f85b7450857203b3a77e22e08
prdimitrov <122304481+prdimitrov@users.noreply.github.com>
2024-02-26 11:04:44 +0200

    Add files via upload

---
+++ Spring Data/_14_EX_Spring_Data_Advanced_Querying/src/main/java/com/example/springintro/model/entity/AuthorNamesWithTotalCopies.java
@@ -0,0 +1,8 @@
+package com.example.springintro.model.entity;
+
+public interface AuthorNamesWithTotalCopies {
+    String getFirstName();
+    String getLastName();
+    long getTotalCopies();
+}
+
